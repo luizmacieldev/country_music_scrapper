f791c79e0f02d6155ce88c1e9b5837f4dd863090
Luiz Maciel <lgamjoeperry@gmail.com>
2024-10-27 15:53:52 -0300

    Added functions and refactored items

--- country/items.py
+++ country/items.py
@@ -1,21 +1,43 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy.loader.processors import Identity, TakeFirst, MapCompose
+from country.utils.common import clean_text, remove_new_lines
 
 class ArtistItem(scrapy.Item):
     _id = scrapy.Field()
-    name = scrapy.Field()
-    listeners = scrapy.Field()
-    avatar_url = scrapy.Field()
-    mini_bio = scrapy.Field()
-    artist_url = scrapy.Field()
-    principal_tags = scrapy.Field()
-    ten_most_played_songs = scrapy.Field()
-    photos = scrapy.Field()
-    most_popular_albums = scrapy.Field()
-    social_media_links = scrapy.Field()
-    biography = scrapy.Field()
-    
+    name = scrapy.Field(
+        input_processor=MapCompose(clean_text),  
+        output_processor=TakeFirst()              
+    )
+    listeners = scrapy.Field(
+        input_processor=MapCompose(int),       
+        output_processor=TakeFirst()
+    )
+    avatar_url = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    mini_bio = scrapy.Field(
+        input_processor=MapCompose(remove_new_lines, clean_text),
+        output_processor=TakeFirst()
+    )
+    artist_url = scrapy.Field(
+        output_processor=TakeFirst()
+    )
+    principal_tags = scrapy.Field(
+        output_processor=Identity()
+    )
+    ten_most_played_songs = scrapy.Field(
+        output_processor=Identity()
+    )
+    photos = scrapy.Field(
+        output_processor=Identity()
+    )
+    most_popular_albums = scrapy.Field(
+        output_processor=Identity()
+    )
+    social_media_links = scrapy.Field(
+        output_processor=Identity()
+    )
+    biography = scrapy.Field(
+        input_processor=MapCompose(remove_new_lines, clean_text),
+        output_processor=TakeFirst()
+    )
